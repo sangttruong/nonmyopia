38cd6e12758a77bc8e0aff80a14b167b0f8dbe86
painkillernhat <painkiller.nhat@gmail.com>
2024-05-04 08:28:29 -0700

    Fix wm

--- llm_sequence_design/world_model.py
+++ llm_sequence_design/world_model.py
@@ -15,7 +15,8 @@ from src.llmtuner.data.preprocess import preprocess_oracle_dataset
 from src.llmtuner.extras.callbacks import FixValueHeadModelCallback
 from src.llmtuner.extras.misc import fix_valuehead_checkpoint
 from src.llmtuner.extras.ploting import plot_loss
-from src.llmtuner.train.oracle.metric import compute_rmse
+from src.llmtuner.train.oracle.metric import compute_regression_metrics
+from src.llmtuner.train.oracle.trainer import OracleTrainer
 
 from utils import get_dataset_embedding
 
@@ -140,13 +141,13 @@ class WorldModel:
         wm_training_args.remove_unused_columns = False  # important for pairwise dataset
 
         # Initialize our Trainer
-        trainer = WMTrainer(
+        trainer = OracleTrainer(
             model=self.model,
             args=wm_training_args,
             finetuning_args=self.finetuning_args,
             tokenizer=self.tokenizer,
             callbacks=callbacks + [FixValueHeadModelCallback()],
-            compute_metrics=compute_rmse,
+            compute_metrics=compute_regression_metrics,
             train_dataset=dataset,
         )
 
@@ -163,103 +164,3 @@ class WorldModel:
         if trainer.is_world_process_zero() and self.finetuning_args.plot_loss:
             plot_loss(wm_training_args.output_dir, keys=[
                 "loss", "eval_loss", "eval_rmse"])
-
-
-class WMTrainer(Trainer):
-    r"""
-    Inherits Trainer to compute pairwise loss.
-    """
-
-    def __init__(self, finetuning_args: "FinetuningArguments", **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.finetuning_args = finetuning_args
-        self.can_return_loss = True  # override property to return eval_loss
-
-    def create_optimizer(self) -> "torch.optim.Optimizer":
-        if self.optimizer is None:
-            self.optimizer = create_custom_optimzer(
-                self.model, self.args, self.finetuning_args)
-        return super().create_optimizer()
-
-    def create_scheduler(
-        self, num_training_steps: int, optimizer: Optional["torch.optim.Optimizer"] = None
-    ) -> "torch.optim.lr_scheduler.LRScheduler":
-        create_custom_scheduler(self.args, num_training_steps, optimizer)
-        return super().create_scheduler(num_training_steps, optimizer)
-
-    def compute_loss(
-        self,
-        model: "PreTrainedModel",
-        inputs: Dict[str, torch.Tensor],
-        return_outputs: Optional[bool] = False
-    ) -> Union[torch.Tensor, Tuple[torch.Tensor, List[torch.Tensor]]]:
-        r"""
-        Computes loss.
-
-        Subclass and override to inject custom behavior.
-
-        Note that the first element will be removed from the output tuple.
-        See: https://github.com/huggingface/transformers/blob/v4.30.2/src/transformers/trainer.py#L3509
-        """
-        # Compute rewards
-        _, _, values = model(
-            input_ids=inputs["input_ids"],
-            attention_mask=inputs["attention_mask"],
-            output_hidden_states=True,
-            return_dict=True)
-
-        unwrapped_model: "PreTrainedModel" = self.accelerator.unwrap_model(
-            self.model)
-        if getattr(unwrapped_model.config, "model_type", None) == "chatglm":
-            values = torch.transpose(values, 0, 1)
-
-        # Split the inputs and rewards into two parts, chosen and rejected
-        batch_size = inputs["input_ids"].size(0)
-        input_ids = inputs["input_ids"]
-        real_rewards = inputs["rewards"]
-        rewards = values
-        scores = []
-
-        # Compute pairwise loss. Only backprop on the different tokens before padding
-        # Inspired by: https://github.com/CarperAI/trlx/blob/main/examples/summarize_rlhf/reward_model/reward_model.py
-        loss = 0
-        for i in range(batch_size):
-            end_index = length = (
-                input_ids[i] != self.tokenizer.pad_token_id).nonzero()[-1] + 1
-            div_index = end_index - 1
-
-            assert div_index > 0
-            trunc_rewards = rewards[i, div_index:end_index]
-            if return_outputs:  # use the score on the last token except pad token for inference
-                scores.append(rewards[i, length-1])
-            loss += torch.nn.functional.mse_loss(trunc_rewards,
-                                                 real_rewards[i].unsqueeze(0),
-                                                 reduction='mean').mean()
-
-        loss = loss / batch_size
-        if return_outputs:
-            scores = torch.stack(scores)
-            return loss, [loss, scores]
-
-        return loss
-
-    def save_predictions(self, predict_results: "PredictionOutput") -> None:
-        r"""
-        Saves model predictions to `output_dir`.
-
-        A custom behavior that not contained in Seq2SeqTrainer.
-        """
-        if not self.is_world_process_zero():
-            return
-
-        output_prediction_file = os.path.join(
-            self.args.output_dir, "generated_predictions.jsonl")
-        logger.info(f"Saving prediction results to {output_prediction_file}")
-        chosen_scores, rejected_scores = predict_results.predictions
-
-        with open(output_prediction_file, "w", encoding="utf-8") as writer:
-            res: List[str] = []
-            for c_score, r_score in zip(chosen_scores, rejected_scores):
-                res.append(json.dumps(
-                    {"chosen": round(float(c_score), 2), "rejected": round(float(r_score), 2)}))
-            writer.write("\n".join(res))
