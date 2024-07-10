b5f4914d7cfdac9091d0a8157e0128443dc57559
lmorlok <luca.morlok@icloud.com>
2024-07-10 11:56:10 -0700

    Implemented function "get_embeddings". Already tested --> Working! :)

--- sequence_design/utils.py
+++ sequence_design/utils.py
@@ -114,6 +114,40 @@ def compute_regression_metrics(eval_preds: Sequence[Union[np.array, Tuple[np.arr
             "rmse": root_mean_squared_error(labels, preds)}
 
 
+def get_embeddings(SentenceBatches,model,tokenizer):
+    # Input:    - SentenceBathes: A list of batches of sentences as list(list(str))
+    #             one sentence is one string str.
+    #           - model: as defined in llmtuner.model
+    #           - tokenizer: as defined in llmtuner.model
+    # Output:   - EmbeddingBatches: Embeddings of each Sentence 
+    #             according to SentenceBatches structure: list(list(embeddings))
+    # Explanation: Function converts sentences into tokens and gets their embedding.
+    #             Can be called with multiple batches containing multiple sentences.
+    
+    EmbeddingBatches = []
+    BatchEmbedding = []
+
+    for batch in SentenceBatches:
+        for sentence in batch:
+            ## Get Tokens of sentence
+            SentenceTokens = tokenizer(sentence)["input_ids"]
+
+            ## Get Embeddings (hiddenstate of last input)
+            # make sure, model inputs are on same device as model 
+            # Attention mask is just a vector of ones, since we want attention on all tokens
+            InpIds=torch.tensor([SentenceTokens],device=model.model.device)
+            AttMask=torch.tensor([[1] * len(SentenceTokens)],device=model.model.device)
+
+            # get the actual embedding by calling the forward function of the main model. 
+            # Model consists of model.model("main model") and model.lm_head ("linear adapter")
+            SentenceEmbedding = model.model.forward(input_ids=InpIds,attention_mask=AttMask)\
+                                .last_hidden_state[0][-1].squeeze().detach().cpu().tolist()
+
+            # Now just handle list structure.
+            BatchEmbedding.append(SentenceEmbedding)
+        EmbeddingBatches.append(BatchEmbedding)
+    return EmbeddingBatches
+
 def get_dataset_embedding(dataset, model, tokenizer, data_args):
 
     preprocess_func = partial(
