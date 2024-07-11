a1f44af2d4f3e40e3c573bded8e3b7c3e9022243
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-07-11 09:28:02 -0700

    Integrate acquisition function testing into

--- sequence_design/bayesian_ridge.py
+++ sequence_design/bayesian_ridge.py
@@ -111,6 +111,9 @@ class BayesianRidgeModel(BatchedMultiOutputGPyTorchModel):
 
 
 if __name__ == "__main__":
+    #========================================
+    #=======   Test regression   ============
+    #========================================
     from sklearn.datasets import fetch_openml
 
     co2 = fetch_openml(data_id=41187, as_frame=True)
@@ -193,10 +196,75 @@ if __name__ == "__main__":
     )
     plt.savefig("bayesian_ridge_regression.png")
 
+
+    #========================================
     # Test condition_on_observations function
+    #========================================
     new_model = model.condition_on_observations(
         X=torch.tensor(X),
         Y=torch.tensor(y),
         input_transform=Normalize(d=1, bounds=torch.tensor([[0], [2050]])),
         # outcome_transform=Standardize(1),
     )
+
+    #========================================
+    #== Test BoTorch aquisition functions ===
+    #========================================
+    from torch.distributions.multivariate_normal import MultivariateNormal as TorchMultivariateNormal
+    import torch
+    import botorch
+    import random
+    from botorch.models import SingleTaskGP
+    from botorch.fit import fit_gpytorch_model
+    from botorch.utils import standardize
+    from gpytorch.mlls import ExactMarginalLogLikelihood
+    import numpy as np
+
+    seed = 3
+    torch.manual_seed(seed=seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    botorch.utils.sampling.manual_seed(seed=seed)
+    
+    train_X = torch.rand(10, 2)
+    Y = 1 - torch.norm(train_X - 0.5, dim=-1, keepdim=True)
+    Y = Y + 0.1 * torch.randn_like(Y)  # add some noise
+    train_Y = standardize(Y)
+    
+    test_X = torch.rand(1, 1, 2)
+    
+    # model = SingleTaskGP(train_X, train_Y)
+    # mll = ExactMarginalLogLikelihood(model.likelihood, model)
+    # fit_gpytorch_model(mll)
+    
+    model = BayesianRidgeModel(train_X, train_Y)
+    
+    best_f = train_Y.max()
+    sampler = botorch.sampling.SobolQMCNormalSampler(10000)
+    
+    qEI = botorch.acquisition.monte_carlo.qExpectedImprovement(model, best_f, sampler)
+    qei = qEI(test_X)
+    print("qEI:", qei)
+    
+    qPi = botorch.acquisition.monte_carlo.qProbabilityOfImprovement(model, best_f, sampler)
+    qpi = qPi(test_X)
+    print("qPI:", qpi)
+    
+    print("Test X:", test_X)
+    print("Posterior mean:", model.posterior(test_X).mean)
+    print("Posterior variance:", model.posterior(test_X).variance)
+    
+    
+    dist = TorchMultivariateNormal(
+        model.posterior(test_X).mean, model.posterior(test_X).variance
+    )
+    
+    samples = [torch.relu(dist.sample() - best_f) for _ in range(10000)]
+    mean = torch.mean(torch.stack(samples), dim=0)
+    print("MC-EI:", mean)
+    
+    
+    samples = [torch.sigmoid((dist.sample() - best_f) / 1e-3) for _ in range(10000)]
+    mean = torch.mean(torch.stack(samples), dim=0)
+    print("MC-PI:", mean)
+
