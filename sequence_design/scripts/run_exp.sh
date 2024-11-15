98e5a3d88101ba5d8682ae38bc37885ac40ea3a4
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-11-15 12:51:35 -0800

    Update script for sequence design

--- sequence_design/scripts/run_exp.sh
+++ sequence_design/scripts/run_exp.sh
@@ -2,6 +2,10 @@
 python emb_server.py --model google/gemma-7b --host 0.0.0.0 --port 1337 --batch_size=8
 
 # Sample experiment scripts
+## First export machine type and mutant version
+export MACHINE=ampere
+export MUTANT=v1
+
 ## Myopic EI
 python main.py --config configs/ei-1seq-64rs-s42.yaml
 
